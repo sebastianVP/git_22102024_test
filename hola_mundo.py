4d9f11932b3de0f67c9b4c6aed5b9a8cd548ccf8
sebastianVP <alex.valdezp22@gmail.com>
2024-10-24 21:57:30 -0500

    Update archivo hola_mundo.py en la Rama Development

--- hola_mundo.py
+++ hola_mundo.py
@@ -3,6 +3,7 @@ def saludo():
     print("Hola, mundo!")
     print("SESION 2 DE GIT CAMBIOS EN EL REPOSITORIO REMOTO")
     print("SESION 2 DE GIT CAMBIOS EN EL REPOSITORIO REMOTO DESDE WINDOWS")
+    print("SESION 2 DE GIT CAMBIOS REALIZADOS EN EL BRANCH - OJO EN EL BRANCHS")
 
 if __name__ == "__main__":
     saludo()
