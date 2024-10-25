732b14e1505a21c1be50a1563517976e1594d193
Alexander Valdez Portocarrero <alex.valdezp22@gmail.com>
2024-10-24 22:02:38 -0500

    Update hola_mundo.py desde el repositorio remoto branch development

--- hola_mundo.py
+++ hola_mundo.py
@@ -4,6 +4,7 @@ def saludo():
     print("SESION 2 DE GIT CAMBIOS EN EL REPOSITORIO REMOTO")
     print("SESION 2 DE GIT CAMBIOS EN EL REPOSITORIO REMOTO DESDE WINDOWS")
     print("SESION 2 DE GIT CAMBIOS REALIZADOS EN EL BRANCH - OJO EN EL BRANCHS")
+    print("SESION 2 CAMBIOS en el repositorio remoto UPDATE")
 
 if __name__ == "__main__":
     saludo()
