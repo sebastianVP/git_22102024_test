3bd5a225625c950247a478cebf2ad77c2baa6aa8
sebastianVP <alex.valdezp22@gmail.com>
2024-10-29 21:45:20 -0500

    nueva tarea

--- gestor_tareas.py
+++ gestor_tareas.py
@@ -1,6 +1,6 @@
 # gestor_tareas.py
 
-tareas=["Estudiar GIT", "Completar el trabajo de clase"]
+tareas=["Estudiar GIT", "Completar el trabajo de clase","Enviar al correo el repositorio de github"]
 
 def listar_tareas():
     for tarea in tareas:
